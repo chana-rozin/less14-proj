d42b84e928b6a247a50871b4ec1cf509d02c0ee2
chana-rozin <c0556787069@gmail.com>
2024-10-30 09:58:26 +0200

    add .gitignor

--- front/src/components/Card/Card.jsx
+++ front/src/components/Card/Card.jsx
@@ -1,35 +1,60 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import ColorPicker from '../ColorPicker/ColorPicker';
-import style from './Card.module.css'
+import style from './Card.module.css';
 import api from '../../service/cards';
 
-function Card({data}) {
-
+function Card({ data }) {
   const [isInEditing, setIsInEditing] = useState(false);
-  const [card, setCard] = useState({});
+  const [card, setCard] = useState(data || {});
 
-  useEffect(() => setCard(data),[data]);
+  useEffect(() => {
+    setCard(data);
+  }, [data]);
 
-  const saveChanges  = ()=>{
+  const saveChanges = () => {
     api.updateCard(card.id, card);
   };
 
-  const toggleState = () => {
-    if(isInEditing)
-      saveChanges();
-    setIsInEditing(!isInEditing);
-  }
-
   const handleChangeColor = (color) => {
-    setCard({...card, color: color});
+    setCard({ ...card, color });
+  };
+
+  const handleInputChange = (event) => {
+    setCard({ ...card, body: event.target.value });
+  };
+
+  const enableEditing = () => {
+    setIsInEditing(true);
+  };
+
+  const handleBlur = () => {
+    saveChanges();
+    setIsInEditing(false);
   };
 
   return (
-    <div className={`${style.card} ${style[data.color]}`}>
-        <input type='text' disabled={!isInEditing} onClick={()=>toggleState()} value={data.body}></input>
-        <ColorPicker color = {data.color} handleChangeColor={handleChangeColor}/>
+    <div className={`${style.card} ${style[card.color]}`}>
+      {isInEditing ? (
+        <input
+          type="text"
+          autoFocus
+          onBlur={handleBlur}
+          value={card.body || ''}
+          onChange={handleInputChange}
+        />
+      ) : (
+        <span
+          onClick={enableEditing}
+          title="Click to edit" // Displays "Click to edit" on hover
+          className={style.cardText}
+        >
+          {card.body || 'Click to edit'}
+        </span>
+      )}
+      
+      <ColorPicker color={card.color} handleChangeColor={handleChangeColor} />
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
